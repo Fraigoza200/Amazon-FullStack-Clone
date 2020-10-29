7247e8408aa68da030f9ce8c3984602d9753762a
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-10-29 10:06:53 -0700

    Firebase Config, Header, Home comp created

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAYzj0eXIlZCCRwXih4epgLXCjMCxmdWuU",
+    authDomain: "clone-1e1fc.firebaseapp.com",
+    databaseURL: "https://clone-1e1fc.firebaseio.com",
+    projectId: "clone-1e1fc",
+    storageBucket: "clone-1e1fc.appspot.com",
+    messagingSenderId: "569452303069",
+    appId: "1:569452303069:web:369023b47dca5d89ed0134"
+  };
