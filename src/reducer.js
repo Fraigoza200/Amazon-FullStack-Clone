249e5d9dd100a16d3dfc8d3159a8d898dd5aa20f
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-10-29 14:38:36 -0700

    adding logic to checkout page

--- src/reducer.js
+++ src/reducer.js
@@ -5,3 +5,13 @@ export const initialState = {
 
 export const getBasketTotal = basket => 
 basket?.reduce((amount, item) => item.price + amount, 0)
+
+const reducer = (state, action) => {
+    console.log(action)
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default reducer
