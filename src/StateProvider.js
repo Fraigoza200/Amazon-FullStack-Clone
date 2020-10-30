287af412cd8ff864db31a9225aed7f74c59d9838
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-10-30 15:33:16 -0700

    install react router added state logic to checkout

---
+++ src/StateProvider.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StateProvider } from './reducer'
+
+function StateProvider() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default StateProvider
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reducer from './reducer'
+import { initialState } from './reducer'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
