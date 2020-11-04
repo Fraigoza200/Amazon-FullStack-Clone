06395cd97054ef747fe1f91f4d7eb8a9ba891c9b
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-11-04 09:56:44 -0800

    creating Login Page

--- src/reducer.js
+++ src/reducer.js
@@ -16,7 +16,7 @@ const reducer = (state, action) => {
             return {
                 ...state, basket: [...state.basket, action.item]
             }
-
+ 
 
         case 'REMOVE_FROM_BASKET':
             const index = state.basket.findIndex((basketItem) => basketItem.id === action.id)
@@ -29,7 +29,7 @@ const reducer = (state, action) => {
             }
 
             return {...state, basket: newBasket}
-            
+
         default: 
             return state
     }
