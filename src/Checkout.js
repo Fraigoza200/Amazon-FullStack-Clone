34a11d63ce8be5c638e5f562ea901ec1fdde308b
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-11-04 11:44:26 -0800

    Name of Guest displayed on header and basket

--- src/Checkout.js
+++ src/Checkout.js
@@ -7,7 +7,7 @@ import CheckoutProduct from './CheckoutProduct'
 
 function Checkout() {
 
-    const [{ basket }, dispatch] = useStateValue()
+    const [{ basket, user }, dispatch] = useStateValue()
 
 
     return (
@@ -17,6 +17,7 @@ function Checkout() {
                  alt="amazon banner"/>
 
                  <div>
+                     <h3> Hello, {user?.email}</h3>
                      <h2 className='checkout__title'>Your shopping Basket</h2>
 
                      {basket.map(item => (
